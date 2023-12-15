141ebbf0a9cc13ed605538dccf49423f77505880
Muqiu Han <muqiu-han@outlook.com>
2023-12-15 12:29:44 +0800

    [Update]: new Sata model

--- src/main/scala/com/muqiuhan/sata/image/object_detection/input/Input.scala
+++ src/main/scala/com/muqiuhan/sata/image/object_detection/input/Input.scala
@@ -3,6 +3,7 @@
 package com.muqiuhan.sata.image.object_detection.input
 
 import com.muqiuhan.sata
+import com.muqiuhan.sata.Transform
 import org.opencv.core.*
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
@@ -10,15 +11,9 @@ import org.opencv.imgproc.Imgproc
 import java.io.File
 import java.nio.file.InvalidPathException
 
-class Transform(image: Mat) extends sata.Transform[Mat, Mat](image):
-  val letterbox: LetterBox = LetterBox(image)
-
-  override def trans(): Mat =
-    val image = letterbox.trans()
-    Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2RGB)
-    image.clone()
-
 case class Input(source: String) extends sata.Input[String, Mat](source):
+
+  val letterbox: LetterBox = LetterBox(image)
 
   val image: Mat =
     if File(source).exists() then
@@ -26,6 +21,7 @@ case class Input(source: String) extends sata.Input[String, Mat](source):
     else
       throw InvalidPathException(source, "Cannot find it")
 
-  val transform: Transform = Transform(image)
-
-  override def input(): Mat = transform.trans()
+  override def input(transform: Transform[Mat, Mat]): Mat =
+    val image = letterbox.trans()
+    Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2RGB)
+    image.clone()
